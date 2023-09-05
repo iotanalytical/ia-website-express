a61ece83f9b1a2f377726e943db07d1d7bf589ab
Ali Sadoughi <asadoughi1990@gmail.com>
2023-09-04 20:10:14 -0400

    add single log delete route

--- routes/logs.js
+++ routes/logs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const db = require("../database/db");
 
+// Get specific user log data
 router.get("/:user", (req, res) => {
   // fetch data from database
   async function run() {
@@ -11,6 +12,27 @@ router.get("/:user", (req, res) => {
       const results = await col.find({user_name: req.params.user}).toArray();
 
       res.status(200).send(results);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+});
+
+
+// Delete a single log
+router.delete("/:id", (req, res) => {
+  // fetch data from database
+  async function run() {
+    try {
+      const col = db.collection("logs");
+      const query = { _id: req.params.id };
+
+      let result = await col.deleteOne(query);
+
+      res.status(200).send(result);
     } catch (err) {
       console.log(err.stack);
     } finally {
